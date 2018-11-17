0ca33b760506f88643c404a6385e5322ae1e8aa9
Chakresh Singh <singh.chakresh@gmail.com>
2018-11-17 14:09:24 -0500

    20181117

---
+++ LeetCode/Easy/paintFence.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int numWays(int n, int k) {
+        
+    }
+};
+int main()
+{
+    Solution obj;
+    cout<<obj.numWays(3,2);
+}
