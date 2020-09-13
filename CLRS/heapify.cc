12ef850b8a2bf39ef97f37b741245fe0104e83f9
chakSingh <singh.chakresh@gmail.com>
2020-09-13 10:54:21 -0400

    heapify-update

--- CLRS/heapify.cc
+++ CLRS/heapify.cc
@@ -3,30 +3,36 @@
 
 using namespace std;
 
+int t = 1;
+
 void max_heap(vector<int> &arr, int i)
 {
     int l, r, largest;
     l = 2*i; r = (2*i + 1);
     largest = i;
+    //cout<<"L: "<<l<<"\t R:"<<r<<"\t i:"<<i<<endl;
     (arr[i] < arr[l] && l<arr.size()) ? largest = l : largest = i;
     if (arr[largest] < arr[r] && r<arr.size())  largest = r;
-    int temp;     
+    if(largest != i) swap(arr[i], arr[largest]);
+    /*int temp;     
     if(largest != i) {
         temp = arr[i];
         arr[i] = arr[largest];
         arr[largest] = temp;
-    }
-    if (largest< arr.size()-2) 
+    }*/
+    if (largest< arr.size() && i!=largest) 
         max_heap(arr, largest);
+    if(i<arr.size())
+        max_heap(arr, ++t);
 
-    arr.erase(arr.begin());
+// arr.erase(arr.begin());
 }
 
 int main()
 {
     //first element in the array is not used.
     vector<int> arr = {0, 27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9};
-    max_heap(arr, 3);
+    max_heap(arr, 1);
     for(int i : arr)
         cout<<i<<" ";
 }
