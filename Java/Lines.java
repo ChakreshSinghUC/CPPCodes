280396d8ee0e895b26bd16fa7c9bb974e5daa93c
wesdoyle <wesdoyle08@gmail.com>
2021-10-05 04:32:46 -0400

    Commit from underground

---
+++ Java/Lines.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.applet.*;
+
+public class Lines extends Applet {
+// public static void main(String[] args){
+    
+// }
+    public void paint (Graphics g){
+        g.drawLine (0,10,100,100);
+    }
+
+}
+
